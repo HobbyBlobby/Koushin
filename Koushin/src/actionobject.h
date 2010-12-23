c126193718d479515c5f329eee4c02f790dc9f87
Felix Lemke <felixlemke@arcor.de>
2010-12-23 23:42:22 +0900

    - remove constructor from ActionParser - write documentation for ActionObject TODO: - continue writing documentation

--- Koushin/src/actionobject.h
+++ Koushin/src/actionobject.h
@@ -29,6 +29,10 @@
  * This class can not used direct.
  **/
 namespace Koushin {
+  /**
+   * @brief This enum provides the information about the type of the derived class.
+   * Often the base class ActionObject is stored or returned. Before a cast to the direved class test the type. Each derived class should have his onw entry in this enum, so do not forget to add it here.
+   **/
   enum actionObjectType{
     actionObjectNotSpecified = 0,
     actionObjectIsBuiling,
@@ -37,6 +41,9 @@ namespace Koushin {
     actionObjectNumber
   };
 
+  /**
+   * @brief A little helper class to store informations about a function for an Action.
+   **/
   class ActionProperties {
     public:
       ActionProperties(QStringList parameterTypes = QStringList(), QString description = "")
@@ -46,17 +53,76 @@ namespace Koushin {
       QString description;
   };
 
+  /**
+   * @brief The basis class for all objects receiving of sending an Action.
+   * To derive from this class you have to implmenent the following functions:
+   * @code
+   * virtual const actionObjectType getActionObjectType()
+   * virtual const QString getLocal(QString name, QString additionalContent = QString())
+   * @endcode
+   * The the documentations belonging to this functions for details.
+   **/
   class ActionObject
   {
     public:
       ActionObject();
-//       virtual ~ActionObject();
       
+      /**
+       * @brief This function helps to find out the derived class.
+       *
+       * @return actionObjectType Each derived class should have a entry in the enum actionObjectType.
+       **/
       virtual const actionObjectType getActionObjectType() = 0; // each class using this class has to return his type
+      /**
+       * @brief The Action can only be executed if this function provides the function name and the right parameters.
+       *
+       * @return QMap<QString, ActionProperties > The QMap contains the name and the corresponding properties.
+       **/
       static const QMap<QString, ActionProperties> getPossibleActions();
       
+      /**
+       * @brief This function replaces a local with a new value.
+       * Each derived class can have his own local parameter (here locals). Every local can either be added the the global or can replace the global.
+       *
+       * @param name The name of the local.
+       * 
+       * @param content The new content for the local.
+       * 
+       * @param replaceGlobal Decide, if the local replaces the global (true) or adds content to a global (false). Default is false.
+       * 
+       * @return bool Like every action this function has to return a bool, indicating the succes of the execution.
+       **/
       bool setLocalTo(QString name, QString content, bool replaceGlobal = false);
+      /**
+       * @brief This function add content to a local.
+       * Each derived class can have his own local parameter (here locals). Every local can either be added the the global or can replace the global.
+       *
+       * @param name The name of the local.
+       * 
+       * @param content The additional content for the local.
+       * 
+       * @param replaceGlobal Decide, if the local replaces the global (true) or adds content to a global (false). Default is false.
+       * 
+       * @return bool Like every action this function has to return a bool, indicating the succes of the execution.
+       **/
       bool addToLocal(QString name, QString content, bool replaceGlobal = false);
+      /**
+       * @brief Returns the local parameter.
+       * This final function should decide, if the parameter a global replacement or an additional content. If the parameter is an additional content add the content from the next "heigher" class (e.g. Town is the next heigher class for Building). As example the implementation from the building:
+       * @code
+       *   if(m_globalReplacements.contains(name))
+       *     return m_globalReplacements.value(name);
+       *   if(m_globalAdditions.contains(name))
+       *     additionalContent += m_globalAdditions.value(name);
+       *   return m_town->getLocal(name, additionalContent);
+       * @endcode
+       *
+       * @param name The name of the local.
+       * 
+       * @param additionalContent The optional additional content from a lower class. Default is QString().
+       * 
+       * @return const QString The content of the local.
+       **/
       virtual const QString getLocal(QString name, QString additionalContent = QString()) = 0;
       
     protected:
--- Koushin/src/actionparser.h
+++ Koushin/src/actionparser.h
@@ -70,14 +70,6 @@ namespace Koushin {
    **/
   class ActionParser {
     public:
-      /**
-       * @brief Constructor
-       *
-       * @param owner The owner is user to parse the recipient. Only with the owner it is possible to find the "current" player or town. Possible owners are in the moment only \class Building s.
-       **/
-//       ActionParser(ActionOwner* owner);
-//       virtual ~ActionParser();
-      
       static QList<Action* > createActionsFromConfig(const KConfigGroup& tasksGroup, ActionObject* newOwner);
       static QPair< QString, QStringList > separateNameAndParameters(QString string);
       static QList<ActionObject* > parseRecipient(const QString& configLine, ActionObject* owner);
@@ -86,87 +78,6 @@ namespace Koushin {
       static QList<Player* > findPlayers(QStringList parameters, ActionObject* owner);
       static QList<Town* > findTowns(QStringList parameters, ActionObject* owner, QList<Player* > players);
       static QList<Building* > findBuildings(QStringList parameters, ActionObject* owner, QList<Town* > towns);
-
-      /**
-       * @brief This function organizes the main work.
-       *Organizing the main work means, that this function read the appropriate keys from the KConfigGroup \c config and gives it to the correct functions. It is necessary to keep the right order of parsing. For example it is not possible to parse the action before the recipient, because only with a recipient the Action can be created.
-       * 
-       * @param config This KConfigGroup should contain another group called \b tasks. 
-       * 
-       * @return :Action* > The returned Action contains all parsed configurations given by \c config.
-       **/
-//       QList<Action* > parseConfig(const KConfigGroup& config);
-    /**
-     * @brief This functions separates a function name and his parameters.
-     *
-     * @param string This string normally has the form <tt>functionName(para1, para2)<tt>.
-     * 
-     * @return QPair< QString, QStringList > The QPair contains the function name and a list of all parameters.
-     **/
-  private:
-    /**
-     * @brief This function finds the right recipient for the Action. 
-     * Using the owner it is possible to find the correct recipient. Depending on the owner type the result varies. For example for a player as owner type (e.g. for magic rituals [not implemented yet]) the string \c{town(current)} does not make any sense.
-     *
-     * @param configLine The relevant line from the configuration file.
-     * 
-     * @return bool The status of parsing the recipient. \c false means, it is not possible to find a reasonable recipient.
-     **/
-    /**
-     * @brief This function parses the action name.
-     * The string is divided into his name and his parameters. This function also tests, if the name is in the list of possible actions (provided by the corresponding Action class) and if the number of parameters is correct.
-     * \todo move the priority into an extra function, and insert call into \c parseConfig().
-     *
-     * @param actionString The name line from the configuration file containing the action name.
-     * 
-     * @param priority The priority of the tasks. The standard value given by the \c parseConfig() function is 10. A higher priority causes a earlier execution. \b Warning: the execution order is influenced by the conditions.
-     * 
-     * @return bool The status of parsing. \c false means, it is not possible to parse action (e.g. the parameter number is given or the action name is unknown).
-     **/
-//     bool parseAction(const QString& actionString, int priority);
-    /**
-     * @brief The function parses the global variables.
-     * The global variables (here called \b globals) provide a system to influence other actions. Before you use globals, it is necessary to register the globals in the ActionManager (provided by the \c owner). This function parses the configuration groups called \c globals, given in the configuration file like here:
-     * \code
-     * [tasks][globals]
-     * name=5
-     * otherName=10+#name#
-     * \endcode
-     *To use later the globals you have to enclose the global within two hash keys, like <tt>\#name\#</tt>.
-     * 
-     * @param parameterList The list contains the whole <tt>[globals]</tt> group. The left site of the equals sign becomes the name of the global, the right site is stored as content. The content can also use other globals.
-     * 
-     * @return bool The status of parsing. There is nothing to check, so it will always return \c true.
-     **/
-    /**
-     * @brief This function searches a reasonable player.
-     * The behaviour of this function depends on the given owner type. In that moment only the Building is implemented as ActionOwner. For this type the player will result from the the owner of the Town the Building belongs to.
-     * Later it should by possible to influence other players (e.q. with a spell or technology).
-     *
-     * @param parameter As parameter you can give special commands to influence the finding process. Now only a empty string or \c current is known (both do the same).
-     * 
-     * @return bool The status of parsing. Of it is possible to find a player then the function return \c true, else \c false.
-     **/
-//     bool findPlayer(const QString& parameter);
-    /**
-     * @brief This function searches a reasonable town.
-     * Later it should by possible to influence other players (e.q. with a spell or technology).
-     *
-     * @param parameter As parameter you can give special commands to influence the finding process. Now only a empty string or \c current is known (both do the same).
-     * 
-     * @return bool The status of parsing. Of it is possible to find a player then the function return \c true, else \c false.
-     **/
-//     bool findTown(const QString& parameter);
-    /**
-     * @brief This functions parses the conditions for actions.
-     * It is possible to add conditions to actions. Theses conditions can influence the order of executing actions. Independent from the priority the actions waits until the requirements are executed. You can decide if the actions is executed if the requirement succeeded of failed.
-     *
-     * @param conditionStrings The list contains all lines of the configurations group <tt>[conditions]</tt>. The left side of the equals sign is used as name for the conditions (just for the developer) and the right site contains the condition.
-     * 
-     * @param actions The QMap contains pairs of action name and a pointer to the Action object. With this map it is easier to find Actions by name.
-     * 
-     * @return void
-     **/
   };
 }
 
